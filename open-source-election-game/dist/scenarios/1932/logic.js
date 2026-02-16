2ad398db02b27d0cd0af3548122f6fd8749f43d0
HaydarYilmaz <ozsoymurat387@gmail.com>
2026-02-16 20:18:14 +0300

    Add dist build for GitHub Pages

---
+++ open-source-election-game/dist/scenarios/1932/logic.js
@@ -0,0 +1,53 @@
+function createEnding(engine, results) {
+    if(engine.playerWonEv(results)) {
+        return {
+            slides: [
+                {
+                    endingHeader: "You won!",
+                    imageUrl: "https://placehold.co/250x250",
+                    endingText: "You win electorally speaking!"
+                },
+                {
+                    endingHeader: "You won!",
+                    imageUrl: "https://placehold.co/250x250",
+                    endingText: "Slide 2!"
+                },
+                {
+                    endingHeader: "You won!",
+                    imageUrl: "https://placehold.co/250x250",
+                    endingText: "Slide 3 :3"
+                }
+            ]
+        }
+    }
+    
+    return {
+        slides: [
+            {
+                endingHeader: "You lost!",
+                imageUrl: "https://placehold.co/250x250",
+                endingText: "You lose electorally speaking!"
+            },
+            {
+                endingHeader: "You lost!",
+                imageUrl: "https://placehold.co/250x250",
+                endingText: "Slide 2!"
+            },
+            {
+                endingHeader: "You lost!",
+                imageUrl: "https://placehold.co/250x250",
+                endingText: "Slide 3 :3"
+            }
+        ]
+    }
+}
+
+function onAnswerPicked(engine, answerPicked) {
+   
+}
+
+function onScenarioStarted(engine) {
+    
+}
+
+export {createEnding, onAnswerPicked, onScenarioStarted}
